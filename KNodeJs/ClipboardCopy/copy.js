011bebf1223d528dda4a767113d80015e1ea8f0f
keshavsoft <nknnkumar@yahoo.com>
2024-04-05 09:19:27 +0530

    keshav

--- KNodeJs/ClipboardCopy/copy.js
+++ KNodeJs/ClipboardCopy/copy.js
@@ -1,4 +1,13 @@
+let fs = require("fs");
+
+let data = fs.readFileSync("./pos.json");
+let datajson = JSON.parse(data);
+let LocalNewArray = Object.keys(datajson).map((key) => {
+    return { ...datajson[key], pk: key }
+});
+
 var cp = require("child_process");
 var child = cp.spawn('clip');
-child.stdin.write("\n");
+child.stdin.write(JSON.stringify(LocalNewArray));
 child.stdin.end();
+
